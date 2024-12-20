c56c0a455b6741da54955339549bf4670609c580
王颖川 <58676324+xieeryihe@users.noreply.github.com>
2024-12-20 14:00:25 +0800

    add utg example

---
+++ utg_example/utg.js
@@ -0,0 +1,130 @@
+utg = {
+  "nodes": [
+    {
+      "id": 0,
+      "package": "com.sankuai.meituan",
+      "activity": "com.meituan.android.pt.homepage.activity.MainActivity",
+      "node_count": 204,
+      "label": "MainActivity",
+      "image": "utg_data\\0.png"
+    },
+    {
+      "id": 1,
+      "package": "com.sankuai.meituan",
+      "activity": "com.meituan.msc.modules.container.MSCActivity",
+      "node_count": 174,
+      "label": "MSCActivity",
+      "image": "utg_data\\1.png"
+    },
+    {
+      "id": 2,
+      "package": "com.sankuai.meituan",
+      "activity": "com.sankuai.meituan.search.home.SearchActivity",
+      "node_count": 108,
+      "label": "SearchActivity",
+      "image": "utg_data\\2.png"
+    },
+    {
+      "id": 3,
+      "package": "com.sankuai.meituan",
+      "activity": "com.sankuai.meituan.search.result.SearchResultActivity",
+      "node_count": 102,
+      "label": "SearchResultActivity",
+      "image": "utg_data\\3.png"
+    },
+    {
+      "id": 4,
+      "package": "com.sankuai.meituan",
+      "activity": "com.meituan.traveltools.mrncontainer.HTMRNBaseActivity",
+      "node_count": 325,
+      "label": "HTMRNBaseActivity",
+      "image": "utg_data\\4.png"
+    },
+    {
+      "id": 5,
+      "package": "com.sankuai.meituan",
+      "activity": "com.meituan.picassobox.MTPicassoBoxActivity",
+      "node_count": 103,
+      "label": "MTPicassoBoxActivity",
+      "image": "utg_data\\5.png"
+    }
+  ],
+  "edges": [
+    {
+      "id": 0,
+      "label": "0",
+      "from": 0,
+      "to": 1,
+      "node_id": 20,
+      "bounds": "[34,168][366,244]",
+      "event_type": "click"
+    },
+    {
+      "id": 1,
+      "label": "1",
+      "from": 1,
+      "to": 0,
+      "node_id": 34,
+      "bounds": "[1099,369][1152,422]",
+      "event_type": "click"
+    },
+    {
+      "id": 2,
+      "label": "2",
+      "from": 0,
+      "to": 2,
+      "node_id": 25,
+      "bounds": "[44,269][1090,399]",
+      "event_type": "click"
+    },
+    {
+      "id": 3,
+      "label": "3",
+      "from": 2,
+      "to": 0,
+      "node_id": 10,
+      "bounds": "[0,120][126,274]",
+      "event_type": "click"
+    },
+    {
+      "id": 4,
+      "label": "4",
+      "from": 0,
+      "to": 3,
+      "node_id": 30,
+      "bounds": "[876,276][1082,391]",
+      "event_type": "click"
+    },
+    {
+      "id": 5,
+      "label": "5",
+      "from": 3,
+      "to": 4,
+      "node_id": 21,
+      "bounds": "[0,421][1440,787]",
+      "event_type": "click"
+    },
+    {
+      "id": 6,
+      "label": "6",
+      "from": 4,
+      "to": 5,
+      "node_id": 28,
+      "bounds": "[601,860][1134,922]",
+      "event_type": "click"
+    },
+    {
+      "id": 7,
+      "label": "7",
+      "from": 5,
+      "to": 1,
+      "node_id": 19,
+      "bounds": "[648,970][835,1040]",
+      "event_type": "click"
+    }
+  ],
+  "info": {
+    "nodeNum": 6,
+    "edgeNum": 8
+  }
+};
